7f0c178b03d92ad8a73477b805355a1a6cfadcba
vivian-ng <vivian@maplerain.com>
2019-09-21 16:26:38 +0900

    Added OTA

--- esp32_camera_streaming.ino
+++ esp32_camera_streaming.ino
@@ -1,6 +1,8 @@
 #include "esp_camera.h"
 #include <WiFi.h>
 #include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 //
 // WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
@@ -9,7 +11,7 @@
 
 //#include "credentials.h";
 #define WIFI_SSID "ssid"
-#define WIFI_PASSWORD "ssid_password"
+#define WIFI_PASSWORD "password"
 #define HOSTNAME "esp32cam"
 
 // Wifi settings
@@ -82,7 +84,42 @@ void setup() {
     return;
   }
 
+  sensor_t * s = esp_camera_sensor_get();
+  //s->set_vflip(s, 1); // flip vertically
+  //s->set_brightness(s, 1); //+ve to increase, -ve to decrease
+  //s->set_saturation(s, -2); //+ve to increase, -ve to lower
+  //s->set_hmirror(s, 1); // mirror horizontally
+
   wifi_setup();
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
   if (!MDNS.begin(HOSTNAME)) {
     Serial.println("Error setting up MDNS responder!");
     while(1) {
@@ -100,4 +137,5 @@ void setup() {
 
 void loop() {
   wifi_connection_manager();
+  ArduinoOTA.handle();
 }
